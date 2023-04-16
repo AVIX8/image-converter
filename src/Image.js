a1bfea740915001bd1ad7565125b674d2878f2b1
AVIX <avix256@gmail.com>
2023-04-16 22:11:34 +0700

    feat: работает сохранение изображения

--- src/Image.js
+++ src/Image.js
@@ -1,4 +1,12 @@
 const { createCanvas, loadImage } = require("canvas");
+const path = require("path");
+const fs = require("fs");
+
+const formats = {
+    png: "png",
+    jpg: "jpeg",
+    jpeg: "jpeg",
+};
 
 class Image {
     width = 0;
@@ -15,6 +23,20 @@ class Image {
         this.canvas = createCanvas(this.width, this.height);
         this.ctx = this.canvas.getContext("2d");
         this.ctx.drawImage(image, 0, 0);
+    }
+
+    getFormat(filePath) {
+        const ext = path.extname(filePath).slice(1);
+        if (!(ext in formats)) {
+            throw new Error("Invalid format");
+        }
+        return `image/${formats[ext]}`;
+    }
+
+    async save(outputPath) {
+        const format = this.getFormat(outputPath)
+        const buffer = this.canvas.toBuffer(format);
+        fs.writeFileSync(outputPath, buffer);
     }
 }
 
