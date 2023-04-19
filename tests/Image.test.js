aabef9623ee0352aefcd5453a231267c357726f0
AVIX <avix256@gmail.com>
2023-04-19 15:38:29 +0700

    test: вынес подготовку объектов изображений в хуки

--- tests/Image.test.js
+++ tests/Image.test.js
@@ -1,113 +1,105 @@
-const Image = require('../src/Image.js');
-const path = require('path')
-const fs = require('fs')
+const Image = require("../src/Image.js");
+const path = require("path");
+const fs = require("fs");
 
-const inputDir = path.join(process.cwd(), 'images')
-const outputDir = path.join(process.cwd(), 'out');
+const inputDir = path.join(process.cwd(), "images");
+const outputDir = path.join(process.cwd(), "out");
 
-const catPath = path.join(inputDir, 'cat.jpg');
-const lambaPath = path.join(inputDir, 'lamba.png');
-const trillionsPath = path.join(inputDir, '4trillions.jpg');
+const catPath = path.join(inputDir, "cat.jpg");
+const lambaPath = path.join(inputDir, "lamba.png");
+const trillionsPath = path.join(inputDir, "4trillions.jpg");
 
-const png16 = path.join(inputDir, '16x16.png');
-const png16corrupted = path.join(inputDir, '16x16-2.png');
-const jpg16 = path.join(inputDir, '16x16.jpg');
+const png16 = path.join(inputDir, "16x16.png");
+const png16corrupted = path.join(inputDir, "16x16-2.png");
+const jpg16 = path.join(inputDir, "16x16.jpg");
 
-describe('Image', () => {
-    it('Открытие картинки из файла', async () => {
-        const image = new Image();
-        await image.loadImage(catPath)
+describe("Image", () => {
+    let image1, image2;
+    beforeEach(() => {
+        image1 = new Image();
+        image2 = new Image();
+    });
+    afterEach(() => {
+        image1 = undefined;
+        image2 = undefined;
+    });
 
-        expect(image).toHaveProperty('width')
-        expect(image).toHaveProperty('height')
-        expect(image).toHaveProperty('canvas')
-        expect(image).toHaveProperty('ctx')
+    it("Открытие картинки из файла", async () => {
+        await image1.loadImage(catPath);
 
-        expect(image.width).toBe(990)
-        expect(image.height).toBe(922)
-    })
+        expect(image1).toHaveProperty("width");
+        expect(image1).toHaveProperty("height");
+        expect(image1).toHaveProperty("canvas");
+        expect(image1).toHaveProperty("ctx");
 
-    it('Сохранение изображения', async () => {
-        const image = new Image();
-        await image.loadImage(catPath)
-        const newPath = path.join(outputDir, 'newCat.jpg');
-        await image.save(newPath)
-        expect(fs.existsSync(newPath)).toBe(true)
-    })
+        expect(image1.width).toBe(990);
+        expect(image1.height).toBe(922);
+    });
 
-    describe('Сравнение изображений', () => {
-        it('Одинаковые изображения одного формата', async () => {
-            const image1 = new Image();
-            const image2 = new Image();
+    it("Сохранение изображения", async () => {
+        await image1.loadImage(catPath);
+        const newPath = path.join(outputDir, "newCat.jpg");
+        await image1.save(newPath);
+        expect(fs.existsSync(newPath)).toBe(true);
+    });
+
+    describe("Сравнение изображений", () => {
+        it("Одинаковые изображения одного формата", async () => {
             await image1.loadImage(png16);
             await image2.loadImage(png16);
-            expect(image1.compare(image2)).toBe(true)
-        })
+            expect(image1.compare(image2)).toBe(true);
+        });
 
-        it('Одинаковые изображения разных форматов', async () => {
-            const image1 = new Image();
-            const image2 = new Image();
+        it("Одинаковые изображения разных форматов", async () => {
             await image1.loadImage(png16);
             await image2.loadImage(jpg16);
-            expect(image1.compare(image2)).toBe(true)
-        })
+            expect(image1.compare(image2)).toBe(true);
+        });
 
-        it('Разные изображения одного размера', async () => {
-            const image1 = new Image();
-            const image2 = new Image();
+        it("Разные изображения одного размера", async () => {
             await image1.loadImage(png16);
             await image2.loadImage(png16corrupted);
-            expect(image1.compare(image2)).toBe(false)
-        })
+            expect(image1.compare(image2)).toBe(false);
+        });
 
-        it('Разные изображения разного размера', async () => {
-            const image1 = new Image();
-            const image2 = new Image();
+        it("Разные изображения разного размера", async () => {
             await image1.loadImage(png16);
             await image2.loadImage(catPath);
-            expect(image1.compare(image2)).toBe(false)
-        })
-    })
+            expect(image1.compare(image2)).toBe(false);
+        });
+    });
 
-    describe('Конвертация изображений', () => {
-        it('png в jpg', async () => {
-            const image1 = new Image();
-            const image2 = new Image();
+    describe("Конвертация изображений", () => {
+        it("png в jpg", async () => {
             await image1.loadImage(png16);
-            const newPath = path.join(outputDir, 'jpg16.jpg');
-            image1.save(newPath)
+            const newPath = path.join(outputDir, "jpg16.jpg");
+            image1.save(newPath);
             await image2.loadImage(newPath);
-            expect(image1.compare(image2)).toBe(true)
-        })
+            expect(image1.compare(image2)).toBe(true);
+        });
 
-        it('jpg в png', async () => {
-            const image1 = new Image();
-            const image2 = new Image();
+        it("jpg в png", async () => {
             await image1.loadImage(jpg16);
-            const newPath = path.join(outputDir, 'png16.png');
-            image1.save(newPath)
+            const newPath = path.join(outputDir, "png16.png");
+            image1.save(newPath);
             await image2.loadImage(newPath);
-            expect(image1.compare(image2)).toBe(true)
-        })
+            expect(image1.compare(image2)).toBe(true);
+        });
 
-        it('Крупное изображение (png в jpg)', async () => {
-            const image1 = new Image();
-            const image2 = new Image();
+        it("Крупное изображение (png в jpg)", async () => {
             await image1.loadImage(lambaPath);
-            const newPath = path.join(outputDir, 'lamba.jpg');
-            image1.save(newPath)
+            const newPath = path.join(outputDir, "lamba.jpg");
+            image1.save(newPath);
             await image2.loadImage(newPath);
-            expect(image1.compare(image2)).toBe(true)
-        })
+            expect(image1.compare(image2)).toBe(true);
+        });
 
-        it('Крупное изображение (jpg в png)', async () => {
-            const image1 = new Image();
-            const image2 = new Image();
+        it("Крупное изображение (jpg в png)", async () => {
             await image1.loadImage(trillionsPath);
-            const newPath = path.join(outputDir, '4trillions.png');
-            image1.save(newPath)
+            const newPath = path.join(outputDir, "4trillions.png");
+            image1.save(newPath);
             await image2.loadImage(newPath);
-            expect(image1.compare(image2)).toBe(true)
-        })
-    })
-})
+            expect(image1.compare(image2)).toBe(true);
+        });
+    });
+});
